33018cf9e44dcfa3c13c4c88f0b91ac30ecf0f36
Sumaya003 <118338640+Sumaya003@users.noreply.github.com>
2024-05-09 03:04:43 +0530

    Time: 8 ms (21.69%) | Memory: 12.5 MB (87.83%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int compress(vector<char>& ch) {
+
+        int ct=1,j=0,k=1;
+        vector<int>v;
+        char c=ch[0];
+        for(int i=1;i<ch.size();i++){
+            if(c==ch[i]){
+                ct++;
+            }else{
+                c=ch[i];
+                
+              if(ct>1){
+                  if(ct<=9){ch[k++]=char(ct)+48;}else{
+                      int n=ct/10;
+                      ch[k++]=char(n)+48;
+                      ch[k++]=char(ct%10)+48; }
+              }
+              ch[k++]=c;
+              ct=1;
+            }
+        }
+        int n=ch.size();
+        if(ch[n-1]==ch[n-1]){
+                           if(ct>1){
+                  if(ct<=9){ch[k++]=char(ct)+48;
+                  }else{
+
+                  while(ct!=0){
+                     v.push_back(ct%10);
+                     ct=ct/10;
+                  } 
+                  for(int i=v.size()-1;i>=0;i--) {
+                      ch[k++]=char(v[i])+48;   
+                  } 
+                   
+                    }
+              }
+
+        }
+        for(auto i=ch.begin()+k;i!=ch.end();i++){
+                 ch.erase(i);i--;
+        }
+       
+        int ans=ch.size();
+        return ans;
+    }
+};
